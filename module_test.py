5d3a9562d119870be1900729c9d5ad492d0aba0e
지웅 <92971305+YoonJiWoong@users.noreply.github.com>
2021-12-08 10:48:51 +0900

    Add files via upload
    파이썬 공부 파일

---
+++ module_test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![image.png](attachment:image.png)
+# ![image-2.png](attachment:image-2.png)
+# ![image-3.png](attachment:image-3.png)
+# ![image-4.png](attachment:image-4.png)
+# ![image-5.png](attachment:image-5.png)
+# ![image-6.png](attachment:image-6.png)
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def number_sum(n1,n2):
+    result=n1+n2
+    return result
+
+
+# In[ ]:
+
+
+def cal(num1,num2,op):
+    """덧셈과 뺄셈을 계산하는 함수"""
+    if op=="+":
+        return num1+num2
+    elif op=="-":
+        return num1-num2
+
+
+# In[ ]:
+
+
+def add(op, *args):
+    return sum(args)
+
+
+# In[ ]:
+
+
+def add_sub(num1,num2):
+    return num1+num2, num1-num2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
